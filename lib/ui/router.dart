c12947d3fdb0d100e86477d2b8f73557d07956f8
Kelvin Zhang <me@iamkelv.in>
2019-06-01 23:35:08 +0100

    Complete register view merge [kcz17]

--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -9,8 +9,6 @@ import 'package:frontend/ui/views/register_view.dart';
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case 'login':
-        return MaterialPageRoute<dynamic>(builder: (_) => LoginView());
       case '/items/add':
         return MaterialPageRoute<dynamic>(builder: (_) => ItemCreationView());
       case '/items':
