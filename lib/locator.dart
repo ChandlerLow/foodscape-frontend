1313882df5c7e4c538fafb5f147270a0da63bc78
Viet Nguyen <vhn17@imperial.ac.uk>
2019-06-04 17:19:31 +0100

    Implemented basic editing [vhn17]

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,6 +1,7 @@
 import 'package:frontend/core/models/categories.dart';
 import 'package:frontend/core/services/authentication_service.dart';
 import 'package:frontend/core/view_models/item_creation_model.dart';
+import 'package:frontend/core/view_models/item_editing_model.dart';
 import 'package:frontend/core/view_models/items_model.dart';
 import 'package:frontend/core/view_models/my_list_model.dart';
 import 'package:frontend/core/view_models/register_model.dart';
@@ -21,4 +22,5 @@ void setupLocator() {
   locator.registerLazySingleton(() => ItemsModel());
   locator.registerFactory(() => MyListModel());
   locator.registerFactory(() => ItemCreationModel());
+  locator.registerFactory(() => ItemEditingModel());
 }
--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:frontend/core/models/item.dart';
 import 'package:frontend/ui/views/filter_view.dart';
 import 'package:frontend/ui/views/item_creation_view.dart';
+import 'package:frontend/ui/views/item_editing_view.dart';
 import 'package:frontend/ui/views/item_operation_view.dart';
 import 'package:frontend/ui/views/item_view.dart';
 import 'package:frontend/ui/views/items_view.dart';
@@ -13,12 +14,15 @@ import 'package:frontend/ui/views/register_view.dart';
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case '/items/add':
+        return MaterialPageRoute<dynamic>(builder: (_) => ItemCreationView());
+      case '/items/edit':
+        final Item item = settings.arguments;
+        return MaterialPageRoute<dynamic>(builder: (_) => ItemEditingView(item: item));
       case '/':
         return MaterialPageRoute<dynamic>(builder: (_) => ItemsView());
       case '/items/filter':
         return MaterialPageRoute<dynamic>(builder: (_) => FilterView());
-      case '/items/add':
-        return MaterialPageRoute<dynamic>(builder: (_) => ItemCreationView());
       case '/item':
         final Item item = settings.arguments;
         return MaterialPageRoute<dynamic>(builder: (_) => ItemView(item: item));
