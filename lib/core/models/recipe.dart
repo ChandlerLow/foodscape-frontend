69d4a330cf4a5de9fab492f641e7ac97caf76a55
Chandler Low <Chandler.low@gmail.com>
2019-06-11 19:22:12 +0100

    modified recipe api to take itemMap [tfl4517]

--- lib/core/models/recipe.dart
+++ lib/core/models/recipe.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'item.dart';
+
 class Recipe {
 
-  Recipe({this.recipeTitle, this.imageURL, this.recipeURL, });
+  Recipe({this.recipeTitle, this.imageURL, this.recipeURL});
 
   factory Recipe.fromJson(Map<String, dynamic> json) {
     return Recipe(
@@ -13,5 +17,39 @@ class Recipe {
   final String recipeTitle;
   final String imageURL;
   final String recipeURL;
+
+  /*
+  Recipe({this.recipeTitle, this.imageURL, this.recipeURL, this.missingIngredients, this.usedIngredients});
+
+  factory Recipe.fromJson(Map<String, dynamic> json, Map<String, Item> itemMap) {
+    return Recipe(
+      recipeTitle: json['title'],
+      imageURL: json['image'],
+      missingIngredients: Recipe.getIngredients(json['missedIngredients'], itemMap),
+      usedIngredients: Recipe.getIngredients(json['usedIngredients'], itemMap),
+      recipeURL: 'https://www.imperial.ac.uk/',
+    );
+  }
+
+  final String recipeTitle;
+  final String imageURL;
+  final String recipeURL;
+  final List<Item> missingIngredients;
+  final List<Item> usedIngredients;
+
+
+  static List<Item> getIngredients(List<dynamic> ingredientsList, Map<String, Item> itemMap) {
+    return ingredientsList = ingredientsList.map((dynamic i) => itemMap[Recipe.getIngredient(i)]).toList();
+  }
+
+  static String getIngredient(Map<String, dynamic> ingredient) {
+    String ingredientName = ingredient['name'];
+    if (ingredientName[ingredientName.length - 1] == 's') {
+      ingredientName = ingredientName.substring(0, ingredientName.length - 1);
+    }
+    print(ingredientName);
+    return ingredientName;
+  }
+  */
 
 }
--- lib/core/services/recipe_api.dart
+++ lib/core/services/recipe_api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:frontend/core/models/item.dart';
 import 'package:frontend/core/models/recipe.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
@@ -9,11 +10,14 @@ class RecipeApi {
   static const String endpoint =
       'https://www.food2fork.com/api/search?key=$APIKEY&q=';
 
+
+  static const String spoonEndpoint = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=20&ranking&ignorePantry=true&ingredients=';
+
   Client client = http.Client();
 
   Future<List<Recipe>> getRecipes(String ingredients) async {
    //final Map<String, String> requestHeaders = {'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com', 'X-RapidAPI-Key' : '811ef08dd6msh1e111c963829c02p1a6df3jsn2302f50aa016'};
-   ingredients = ingredients.split(' ').join('%20');
+    ingredients = ingredients.split(' ').join('%20');
     final Response response = await client.get(
       endpoint + ingredients,
     );
@@ -24,10 +28,28 @@ class RecipeApi {
     }
 
     final List<dynamic> recipesJson =
-    json.decode(response.body)['recipes'];
+    json.decode(response.body);
     return recipesJson
         .map((dynamic recipeJson) => Recipe.fromJson(recipeJson))
         .toList();
   }
+/*
+  Future<List<Recipe>> getRecipesFromMap(Map<String, Item> itemMap) async {
 
+    final String ingredients = itemMap.keys.join('%2C');
+
+    final Response response = await client.get(spoonEndpoint + ingredients);
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load recipes - ${response.statusCode} - '
+          '${response.body}');
+    }
+
+    final List<dynamic> recipesJson =
+    json.decode(response.body);
+    return recipesJson
+        .map((dynamic recipeJson) => Recipe.fromJson(recipeJson, itemMap))
+        .toList();
+  }
+  */
 }
