d830182b8310d898322064e6fd252104e2e6aa26
Kelvin Zhang <me@iamkelv.in>
2019-06-13 01:55:24 +0100

    Link item interest from backend [kcz17]

--- lib/core/models/item.dart
+++ lib/core/models/item.dart
@@ -1,16 +1,16 @@
 class Item {
-  Item(
-      {this.id,
-      this.name,
-      this.photo,
-      this.quantity,
-      this.expiryDate,
-      this.description,
-      this.userLocation,
-      this.userPhoneNumber,
-      this.userName,
-      this.categoryId,
-      this.isCollected});
+  Item({this.id,
+    this.name,
+    this.photo,
+    this.quantity,
+    this.expiryDate,
+    this.description,
+    this.interest,
+    this.userLocation,
+    this.userPhoneNumber,
+    this.userName,
+    this.categoryId,
+    this.isCollected});
 
   factory Item.fromJson(Map<String, dynamic> json) {
     return Item(
@@ -20,6 +20,7 @@ class Item {
         quantity: json['quantity'],
         expiryDate: DateTime.parse(json['expiry_date']),
         description: json['description'],
+        interest: json['interest'],
         userLocation: json['user']['location'],
         userPhoneNumber: json['user']['phone_no'],
         userName: json['user']['name'],
@@ -32,6 +33,7 @@ class Item {
   final String name;
   final String photo;
   final String quantity;
+  final int interest;
   final DateTime expiryDate;
   final String description;
   final String userLocation;
